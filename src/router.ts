e8f495544ad0863c1f346a37f088bed8a64c4905
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-21 16:31:56 -0700

    Adding the express-validator dependency

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { registerUser } from './repositories/auth';
+import { handleInputErrors } from './middlewares/validation';
+import { body } from 'express-validator';
 
 const router = Router();
 
 router.post(
     '/auth/register',
+    [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('lastname').notEmpty().withMessage('Lastname is required'),
+        body('username').notEmpty().withMessage('Username is required'),
+        body('email').isEmail().withMessage('Invalid email address'),
+        body('password').isLength({min: 4}).withMessage('Password should be at least 4 characters long'),
+    ],
+    handleInputErrors,
     registerUser
 );
 
