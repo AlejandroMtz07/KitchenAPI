1b6a64e2a5ef44cb4da12816b30184913bc92cd7
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-21 18:23:07 -0700

    Adding the bcrypt dependency

--- src/repositories/auth.ts
+++ src/repositories/auth.ts
@@ -1,13 +1,21 @@
 import { pool } from "../config/db";
 import { Request, Response } from "express";
+import { hashPassword } from "../utils/auth";
 
 export const registerUser = async (req: Request, res: Response) => {
 
     try {
+        //Getting the values from the request 
         const { name, lastname, username, email, password } = req.body;
+
+        //Hashing the password
+        const hashedPassword = await hashPassword(password);
+
+        //Inserting the data in the database
         const [fields, rows] = await pool.query(
             'INSERT INTO users (name,lastname,username,email,password,created_at) values (?,?,?,?,?,now())',
-            [name, lastname, username, email, password]);
+            [name, lastname, username, email, hashedPassword]);
+
         res.status(200).json({fields: fields, rows: rows});
     } catch (error) {
         return res.status(500).send(error)
