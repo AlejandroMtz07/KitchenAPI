e67b176c52cdaaf4f28a7b406089dada55ec0c34
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-01-23 21:26:29 -0700

    Adding cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import router from './router';
 import { json } from 'express';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 
 //Server instance
 const app = express();
+
+//Cors configuration
+app.use(cors(corsConfig));
 
 //Dependency for json reading
 app.use(json());
