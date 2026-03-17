84a7552fcb1fbf8a3a5613ea0e813e4514528274
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-03-17 13:43:22 -0700

    Resolving local SSL problems with nodemailer.

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -5,6 +5,9 @@ export const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_ACCOUNT,
         pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
@@ -14,6 +17,6 @@ export const sendMail = async (to: string, subject: string, text: string, html: 
         to: to,
         subject: subject,
         text: text,
-        html: html
+        html: html,
     })
 }
