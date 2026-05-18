d705fa8779a771b76c6e05cd349fd17d8032ad7c
AlejandroMtz07 <alejandro23juanito@gmail.com>
2026-05-17 21:55:26 -0700

    Another changes.

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,11 +3,18 @@ import {CorsOptions} from 'cors'
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback){
         
-        if(origin === 'https://kitchenrecip.netlify.app/'){
-            callback(null,true);
-        }else{
-            callback(new Error('CORS error'));
+        const allowedOrigin = 'https://kitchenrecip.netlify.app';
+
+        // Permitir requests sin origin (Postman, mobile apps, etc.)
+        if (!origin) {
+            return callback(null, true);
         }
+
+        if (origin === allowedOrigin) {
+            return callback(null, true);
+        }
+
+        return callback(new Error('CORS no permitido'));
 
     }
 }
